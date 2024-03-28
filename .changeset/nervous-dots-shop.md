89ad28d66b23ce7cc65c7baf3d4dcee2545c5846
Danilo Tuler <tuler@pobox.com>
2024-03-27 23:41:59 -0300

    Fix typescript packaging

---
+++ .changeset/nervous-dots-shop.md
@@ -0,0 +1,5 @@
+---
+"@tuler/node-cartesi-machine": patch
+---
+
+fix typescript packaging
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     clean: true,
+    dts: true,
     entry: ["lib/binding.ts"],
+    format: ["cjs", "esm"],
     splitting: false,
     sourcemap: true,
 });
